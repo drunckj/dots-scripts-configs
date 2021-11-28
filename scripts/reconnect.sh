9c884d07c5bde9500552fb284d50d6b4feb25c11
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-11-28 13:40:10 +0530

    Update reconnect.sh

--- scripts/reconnect.sh
+++ scripts/reconnect.sh
@@ -6,15 +6,15 @@ do
     if [ $? -eq 0 ]
     then
         #sudo ip n add 172.16.3.219 lladdr b4:c4:fc:80:00:64 dev wlo1 nud perm
-        echo "Internet is working"
+        printf '\e[1;34m%-6s\e[m\n' "Internet is working"
         sleep 10
     elif [ $? -eq 1 ]
     then
-        echo "Internet is not working. Reconnecting."
+        printf '\e[1;31m%-6s\e[m\n' "Internet is not working. Reconnecting."
         nmcli radio wifi off
         sleep 1
         nmcli radio wifi on
-        sleep 5
+        sleep 10
         #sudo ip n flush all
         sudo ip n add $(ip addr show wlo1  | grep "inet" | cut -b 10-20 | grep "172") lladdr $(ip addr show wlo1 | grep "ether" | cut -b 16-32) dev wlo1 nud perm
 
@@ -24,4 +24,3 @@ do
     fi
     sleep 5
 done
-
