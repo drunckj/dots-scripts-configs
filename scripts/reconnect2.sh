917c11b965d17b8df6c376c6096306c5cbf59c02
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2022-03-22 12:27:49 +0530

    Update reconnect2.sh

--- scripts/reconnect2.sh
+++ scripts/reconnect2.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 
- #!/bin/bash
- 
  reconnect(){
  while :
  do
-     sudo ip n flush all
+#     sudo ip n flush all
      ping -c 3 1.1.1.1
-     if [ $? -eq 0 ]
+     if [ $? -eq 0 ] 
      then
          #sudo ip n add 172.16.3.219 lladdr b4:c4:fc:80:00:64 dev wlo1 nud perm
          printf '\e[1;34m%-6s\e[m\n' "Internet is working"
@@ -15,12 +14,13 @@
      then
          printf '\e[1;31m%-6s\e[m\n' "Internet is not working. Reconnecting."
          sudo ip link set dev wlo1 down
-         sleep 1
          sudo ip link set dev wlo1 up
- 
-         #sudo ip n flush all
-         #sudo ip n add $(ip addr show wlo1  | grep "inet" | cut -b 10-20 | grep "172") lladdr $(ip addr show wlo1 |   grep "ether" | cut -b 16-32) dev wlo1 nud perm
- 
+
+         sudo ip n flush all
+         sudo ip n add 172.16.0.1 lladdr 64:d1:54:82:27:6d dev wlo1 nud perm
+         sudo ip n add $(ip addr show wlo1  | grep "inet" | cut -d "/" -f 1 | cut -d " " -f 6
+) lladdr $(ip addr show wlo1 |   grep "ether" | cut -b 16-32) dev wlo1 nud perm
+
          #sudo ip n
          #sudo ip n flush all
          #sleep 10
@@ -28,7 +28,13 @@
      sleep 5
  done
  }
- mac="b4:c4:fc:80:00:64"
+
+
+#mac="b4:c4:fc:80:00:64"
+mac="80:ad:16:c3:f9:80"
+#mac="9c:ad:97:57:a1:01"
+#mac="06:3f:2c:38:00:5c"
+
    mac_check=$(ip addr show wlo1 |  grep "ether" | cut -b 16-32)
    if [[ "$mac" == "$mac_check" ]]
    then
@@ -38,6 +44,5 @@
        sudo ip link set dev wlo1 down
        sudo ip link set dev wlo1 address $mac
        sudo ip link set dev wlo1 up
-       sleep 10
        reconnect
    fi
